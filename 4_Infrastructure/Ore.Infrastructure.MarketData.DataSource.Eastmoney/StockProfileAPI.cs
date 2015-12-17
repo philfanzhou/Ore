b950c2984735b8d550bff0f79b8dab5cc31928c8
philfanzhou <philfanzhou@hotmail.com>
2015-12-17 22:26:37 +0800

    增加基本信息数据定义

---
+++ 4_Infrastructure/Ore.Infrastructure.MarketData.DataSource.Eastmoney/StockProfileAPI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ore.Infrastructure.MarketData.DataSource.Eastmoney
+{
+    class StockProfileAPI
+    {
+        public IStockProfile GetStockProfile(string stockCode)
+        {
+            // http://f10.eastmoney.com/f10_v2/CompanySurvey.aspx?code=sz000002
+            throw new NotImplementedException();
+        }
+    }
+}
--- 4_Infrastructure/Ore.Infrastructure.MarketData/Matedata/IStockProfile.cs
+++ 4_Infrastructure/Ore.Infrastructure.MarketData/Matedata/IStockProfile.cs
@@ -8,58 +8,144 @@ namespace Ore.Infrastructure.MarketData
     public interface IStockProfile
     {
         /// <summary>
-        /// 交易代码
+        /// 公司名称
         /// </summary>
-        string Code { get; }
-
+        string FullName { get; }
+        /// <summary>
+        /// 公司英文名称
+        /// </summary>
+        string EnglishName { get; }
+        /// <summary>
+        /// 曾用名
+        /// </summary>
+        string NameUsedBefore { get; }
+        /// <summary>
+        /// A股交易代码
+        /// </summary>
+        string CodeA { get; }
+        /// <summary>
+        /// A股证券简称
+        /// </summary>
+        string ShortNameA { get; }
+        /// <summary>
+        /// B股交易代码
+        /// </summary>
+        string CodeB { get; }
+        /// <summary>
+        /// B股证券简称
+        /// </summary>
+        string ShortNameB { get; }
+        /// <summary>
+        /// H股交易代码
+        /// </summary>
+        string CodeH { get; }
+        /// <summary>
+        /// B股证券简称
+        /// </summary>
+        string ShortNameH { get; }
         /// <summary>
         /// 证券交易所
         /// </summary>
         Market Exchange { get; }
-
         /// <summary>
-        /// 证券简称
+        /// 所属行业
         /// </summary>
-        string ShortName { get; }
-
+        string Industry { get; }
         /// <summary>
-        /// 证券全称
+        /// 总经理
         /// </summary>
-        string FullName { get; }
-
+        string GeneralManager { get; }
         /// <summary>
-        /// 上市状态
+        /// 法人代表
         /// </summary>
-        ListStatus ListStatus { get; }
-
+        string LegalRepresentative { get; }
         /// <summary>
-        /// 上市日期
+        /// 董秘
         /// </summary>
-        DateTime ListDate { get; }
-
+        string BoardSecretary { get; }
         /// <summary>
-        /// 摘牌日期
+        /// 董事长
         /// </summary>
-        DateTime DelistDate { get; }
-
+        string Chairman { get; }
+        /// <summary>
+        /// 证券事务代表
+        /// </summary>
+        string SecuritiesAffairsRepresentatives { get; }
+        /// <summary>
+        /// 独立董事
+        /// </summary>
+        string IndependentDirectors { get; }
+        /// <summary>
+        /// 联系电话
+        /// </summary>
+        string ContactNumber { get; }
+        /// <summary>
+        /// 电子信箱
+        /// </summary>
+        string Email { get; }
+        /// <summary>
+        /// 传真
+        /// </summary>
+        string Fax { get; }
+        /// <summary>
+        /// 公司网址
+        /// </summary>
+        string Website { get; }
         /// <summary>
         /// 办公地址
         /// </summary>
         string OfficeAddress { get; }
-
+        /// <summary>
+        /// 注册地址
+        /// </summary>
+        string RegisteredAddress { get; }
+        /// <summary>
+        /// 区域
+        /// </summary>
+        string Area { get; }
+        /// <summary>
+        /// 邮政编码
+        /// </summary>
+        string ZipCode { get; }
+        /// <summary>
+        /// 注册资本(元)
+        /// </summary>
+        decimal RegisteredCapital { get; }
+        /// <summary>
+        /// 工商登记
+        /// </summary>
+        string BusinessRegistration { get; }
+        /// <summary>
+        /// 雇员人数
+        /// </summary>
+        int NumberOfEmployees { get; }
+        /// <summary>
+        /// 管理人员人数
+        /// </summary>
+        int NumberOfManagement { get; }
+        /// <summary>
+        /// 律师事务所
+        /// </summary>
+        string LawOffice { get; }
+        /// <summary>
+        /// 会计师事务所
+        /// </summary>
+        string AccountingFirm { get; }
+        /// <summary>
+        /// 公司简介
+        /// </summary>
+        string CompanyProfile { get; }
         /// <summary>
         /// 主营业务范围
         /// </summary>
         string PrimeBusiness { get; }
-
         /// <summary>
-        /// 财报日期
+        /// 成立日期
         /// </summary>
-        DateTime FinancialReportDate { get; }
-
+        DateTime EstablishmentDate { get; }
         /// <summary>
-        /// 所有者权益合计
+        /// 上市日期
         /// </summary>
-        double ShareholderEquity { get; }
+        DateTime ListDate { get; }
     }
 }
