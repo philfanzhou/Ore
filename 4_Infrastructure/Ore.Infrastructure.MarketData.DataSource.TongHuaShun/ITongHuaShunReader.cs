8aef0128dff733239a4936cefa1b7d720ce7024a
philfanzhou <philfanzhou@hotmail.com>
2015-12-24 11:51:32 +0800

    实现同花顺日线读取与UT

--- 4_Infrastructure/Ore.Infrastructure.MarketData.DataSource.TongHuaShun/ITongHuaShunReader.cs
+++ 4_Infrastructure/Ore.Infrastructure.MarketData.DataSource.TongHuaShun/ITongHuaShunReader.cs
@@ -12,5 +12,12 @@ namespace Ore.Infrastructure.MarketData.DataSource.TongHuaShun
         /// 获取或设置数据所在文件夹
         /// </summary>
         string DataFolder { get; }
+
+        /// <summary>
+        /// 获取日线数据
+        /// </summary>
+        /// <param name="stockCode"></param>
+        /// <returns></returns>
+        IKlineData GetDayKLine(string stockCode);
     }
 }
---
+++ 4_Infrastructure/Ore.Infrastructure.MarketData.DataSource.TongHuaShun/New/DataReaderV2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ore.Infrastructure.MarketData.DataSource.TongHuaShun
+{
+    internal class DataReaderV2 : ITongHuaShunReader
+    {
+        private readonly PathHelper _pathHelper;
+
+        public DataReaderV2(string dataFolder)
+        {
+            _pathHelper = new PathHelper(dataFolder);
+        }
+
+        public string DataFolder
+        {
+            get { return _pathHelper.DataFolder; }
+        }
+
+        public IKlineData GetDayKLine(string stockCode)
+        {
+            DataReader reader = new DataReader();
+            reader.AnalyseDayLineFiles(
+                new[] 
+                {
+                    _pathHelper.ShangHaiDay,
+                    _pathHelper.ShenZhenDay
+                });
+
+            return reader.GetDaylineData(stockCode, DateTime.MinValue);
+        }
+    }
+}
