8c52847c0d407ec5ed400f68f4a530438ba4061b
philfanzhou <philfanzhou@hotmail.com>
2016-01-14 17:33:58 +0800

    新增日线K线接口

---
+++ 4_Infrastructure/Ore.Infrastructure.MarketData.DataSource.Sina/StockKLineApi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ore.Infrastructure.MarketData.DataSource.Sina
+{
+    public class StockKLineApi
+    {
+        public IEnumerable<IStockKLine> GetLatest(IEnumerable<string> stockCodes)
+        {
+            // 每天下午3点20之后，调用StockRealTimeApi.GetData(IEnumerable<string> stockCodes)，
+            // 就可以获取到当天的日线K线。
+            throw new NotImplementedException();
+        }
+    }
+}
