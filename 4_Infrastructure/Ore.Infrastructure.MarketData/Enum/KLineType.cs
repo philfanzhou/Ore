6bb0032777e8aa1f84dd4f2e3d0a15eac9294707
philfanzhou <philfanzhou@hotmail.com>
2016-01-17 22:45:08 +0800

    更新K线周期定义和发布新Dll

--- 4_Infrastructure/Ore.Infrastructure.MarketData/Enum/KLineType.cs
+++ 4_Infrastructure/Ore.Infrastructure.MarketData/Enum/KLineType.cs
@@ -1,11 +1,30 @@
 ﻿namespace Ore.Infrastructure.MarketData
 {
+    /// <summary>
+    /// K线周期枚举
+    /// </summary>
     public enum KLineType
     {
         /// <summary>
         /// 日线
         /// </summary>
-        Daily,
+        Day,
+        /// <summary>
+        /// 周线
+        /// </summary>
+        Week,
+        /// <summary>
+        /// 月线
+        /// </summary>
+        Month,
+        /// <summary>
+        /// 季线
+        /// </summary>
+        Quarter,
+        /// <summary>
+        /// 年线
+        /// </summary>
+        Year,
         /// <summary>
         /// 1分钟线
         /// </summary>
@@ -14,5 +33,17 @@
         /// 5分钟线
         /// </summary>
         Min5,
+        /// <summary>
+        /// 15分钟线
+        /// </summary>
+        Min15,
+        /// <summary>
+        /// 30分钟线
+        /// </summary>
+        Min30,
+        /// <summary>
+        /// 60分钟线
+        /// </summary>
+        Min60
     }
 }
